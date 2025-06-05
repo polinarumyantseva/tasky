bd11f735fe2a74b6bcc68b290ea2308bafebbc6b
polinarumyantseva <polinrumyantseva@gmail.com>
2025-06-05 08:45:20 -0700

    edit dockerfile

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,22 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const routes = require('./routes');
+const path = require('path');
 
 const port = 3005;
 const app = express();
+
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use(cookieParser());
 app.use(express.json());
 
 app.use('/', routes);
+
+// Все остальные запросы → index.html (для React Router)
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 mongoose.connect(process.env.DB_CONNECTION_STRING).then(() => {
 	app.listen(port, () => {
